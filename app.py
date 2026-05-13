ebaabdba2e472cc696e048025663c988217f5c0f
paesloma <58999041+paesloma@users.noreply.github.com>
2026-05-13 16:38:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def crear_enlace_wa(telefono_crudo):
     """Crea el link de WhatsApp con el primer número detectado"""
     numero_unico = extraer_primer_numero(telefono_crudo)
     if not numero_unico: return None
-    mensaje = urllib.parse.quote("MENSAJE PERSONALIZADO HOLA.")
+    mensaje = urllib.parse.quote("Buen dia, tengo una articulo de linea blanca (TCL - HB - RCA - CHIQ). Deseo soliciar una revision tecnica, me podria ayudar con informacion y agendamiento.Gracias")
     return f"https://wa.me/{numero_unico}?text={mensaje}"
 
 try:
